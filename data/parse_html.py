4a017a06abf262be9fa2523d0b1d3e76b07610ff
iejMac <kilianmaciej6@gmail.com>
2021-09-11 00:19:41 -0700

    data: parse_html script

---
+++ data/parse_html.py
@@ -0,0 +1,24 @@
+import os
+from bs4 import BeautifulSoup
+
+data_dir = "test"
+
+scripts = os.listdir(data_dir)
+script_path = os.path.join(data_dir, scripts[5])
+
+with open(script_path, "r") as f:
+  html = f.read()
+
+soup = BeautifulSoup(html, "html.parser")
+pre = soup.find("pre")
+
+string = str(pre)
+print(len(string))
+
+
+
+
+
+
+
+
