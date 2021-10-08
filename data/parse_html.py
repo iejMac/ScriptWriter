27b437001138a6a554c5ef2326186cafc1dd0913
iejMac <kilianmaciej6@gmail.com>
2021-10-07 20:37:56 -0700

    parse_html: fix tabs

--- data/parse_html.py
+++ data/parse_html.py
@@ -3,20 +3,20 @@ import sys
 from bs4 import BeautifulSoup
 
 '''
-	Separating getting HTML and parsing it for now since I'm not certain this is how we want to do it in the end
-	Lack of a unified format might cause problems later
+  Separating getting HTML and parsing it for now since I'm not certain this is how we want to do it in the end
+  Lack of a unified format might cause problems later
 '''
 
 def parse_html(data_dir, out_dir):
-	all_html = os.listdir(data_dir)
-	for html_name in all_html:
-		with open(os.path.join(data_dir, html_name), "r") as html:
-			soup = BeautifulSoup(html, "html.parser")
+  all_html = os.listdir(data_dir)
+  for html_name in all_html:
+    with open(os.path.join(data_dir, html_name), "r") as html:
+      soup = BeautifulSoup(html, "html.parser")
 
-		no_tag = soup.get_text()
-		
-		with open(os.path.join(out_dir, html_name.split(".")[0] + ".txt"), "w+") as f:
-			f.write(no_tag)
+    no_tag = soup.get_text()
+    
+    with open(os.path.join(out_dir, html_name.split(".")[0] + ".txt"), "w+") as f:
+      f.write(no_tag)
 
 if __name__ == "__main__":
   if 4 > len(sys.argv) > 2:
