90c787bb375da8c706d0128a82d04cc946ab120c
iejMac <kilianmaciej6@gmail.com>
2021-10-23 13:58:55 -0700

    data: data processing updates in tokenizer

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -1,55 +1,65 @@
 import os
 import torch
+import random
 from torch.nn.functional import one_hot
 from tokenizers import BertWordPieceTokenizer
 
 class ScriptTokenizer:
   def __init__(self,
                vocab_file="bert-base-uncased-vocab.txt",
-               sequence_length=100):
+               pre_length=100,
+               frag_length=100,
+               post_length=100, 
+               ):
 
 
     self.tokenizer = BertWordPieceTokenizer(vocab_file, lowercase=True)
     self.tokenizer.add_special_tokens(["|<TAB>|", "|<NL>|"])
-    self.sequence_length = sequence_length
+
+    self.pre_length = pre_length
+    self.frag_length = frag_length
+    self.post_length = post_length
 
   def get_vocab_size(self):
     return self.tokenizer.get_vocab_size()
 
-  def encode(self, texts):
+  def encode(self, texts, start_seq=0):
     '''
       Encodes raw text into indices for data
 
+      texts: List(str) - list of Scripts in str form
+             str - singular script in str form
+
+
+      Potential pre-processing steps:
       data = data.replace("    ", "\t") # 4 space tabs
       data = data.replace("  ", "\t") # 4 space tabs
     '''
 
     texts = [texts] if isinstance(texts, str) else texts
-    for i in range(len(texts)):
-      texts[i] = texts[i].replace("\t", "|<TAB>|") # Tab structure gives a lot of information
-      texts[i] = texts[i].replace("\n\n", "|<NL>|") # Empty lines in script
 
-    tokenized_texts = []
+    tokenized_pres = []
+    tokenized_frags = []
+    tokenized_posts = []
 
     for text in texts:
-      tokenized = self.tokenizer.encode(text)
-      ids = tokenized.ids[:self.sequence_length] # cap sequence length at sequence_length tokens
-      while len(ids) < self.sequence_length:
-        ids.append(0)
-      tokenized_texts.append(ids)
-    return torch.tensor(tokenized_texts)
 
-if __name__ == "__main__":
-  data_dir = "./out_dir"
-  test_script_name = os.path.join(data_dir, os.listdir(data_dir)[1])
+      ids = self.get_ids(text)
+      start_ind = start_seq if start_seq is not None else random.randint(0, len(ids))
 
-  with open(test_script_name, "r") as f:
-    test = f.read()
+      # TODO: this gets rid of start/end tokens, maybe this is a problem????
+      pre_ids = ids[start_ind:start_ind + self.pre_length]
+      frag_ids = ids[start_ind + self.pre_length: start_ind + self.pre_length + self.frag_length]
+      post_ids = ids[start_ind + self.pre_length + self.frag_length : start_ind + self.pre_length + self.frag_length + self.post_length]
+      
+      for prefix in ["pre", "frag", "post"]:
+        while len(eval(prefix + "_ids")) < eval("self." + prefix + "_length"):
+          eval(prefix + "_ids").append(0)
+        eval("tokenized_" + prefix + "s").append(eval(prefix + "_ids"))
 
-  tok = ScriptTokenizer()
+    return torch.tensor(tokenized_pres), torch.tensor(tokenized_frags), torch.tensor(tokenized_posts)
 
-  test = test[500:2000]
-  test = test.replace("    ", "\t")
-
-  encoded = tok.encode(test)
-  print(encoded.shape)
+  def get_ids(self, text: str):
+    text = text.replace("\t", "|<TAB>|") # Tab structure gives a lot of information
+    text = text.replace("\n\n", "|<NL>|") # Empty lines in script
+    return self.tokenizer.encode(text).ids
--- model/test.py
+++ model/test.py
@@ -7,32 +7,32 @@ import torch
 from tokenizer import ScriptTokenizer
 
 '''
-mod = model.Transformer(5, 2, 1)
-x = torch.randn((1, 2, 5))
+	ASSUMPTIONS:
+		
+		1. len(pre_frag) == len(frag) == len(post_frag)
 
-out = mod(x)
-print(out)
+
 '''
 
 data_dir = "../data/out_dir"
-test_script_name = os.path.join(data_dir, os.listdir(data_dir)[1])
+test_script_name = os.path.join(data_dir, os.listdir(data_dir)[0])
 
 with open(test_script_name, "r") as f:
 	test = f.read()
 
-tok = ScriptTokenizer("../data/bert-base-uncased-vocab.txt")
 
-test = test[500:2000]
+PRE_LEN = 10
+FRAG_LEN = 10
+POST_LEN = 10
+
+tok = ScriptTokenizer("../data/bert-base-uncased-vocab.txt", pre_length=PRE_LEN, frag_length=FRAG_LEN, post_length=POST_LEN)
+
 test = test.replace("    ", "\t")
+texts = [test, test]
+encoded_pres, encoded_frags, encoded_posts = tok.encode(texts, None)
 
-# texts = [test, test]
-texts = [test, test[600:800]]
-
-encoded = tok.encode(texts)
-
-mod = model.CSFP(784, tok.get_vocab_size(), 100, 512, 2, 4)
-
-lpc, lpf = mod(encoded, encoded, encoded)
+mod = model.CSFP(784, tok.get_vocab_size(), 10, 512, 2, 4)
+lpc, lpf = mod(encoded_pres, encoded_frags, encoded_posts)
 
 print(lpc)
 print(lpf)
