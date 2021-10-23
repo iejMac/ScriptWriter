40c7cf4aee61e2fdd181bc2d94982716d7860a9b
iejMac <kilianmaciej6@gmail.com>
2021-10-23 14:37:09 -0700

    model: training going but no results yet

--- model/test.py
+++ model/test.py
@@ -4,6 +4,8 @@ sys.path.append("../data")
 
 import model
 import torch
+from torch import nn
+from torch import optim
 from tokenizer import ScriptTokenizer
 
 '''
@@ -24,29 +26,29 @@ with open(test_script_name, "r") as f:
 PRE_LEN = 10
 FRAG_LEN = 10
 POST_LEN = 10
-BATCH_SIZE = 5
+
+BATCH_SIZE = 10
+EPOCHS = 50
 
 tok = ScriptTokenizer("../data/bert-base-uncased-vocab.txt", pre_length=PRE_LEN, frag_length=FRAG_LEN, post_length=POST_LEN)
 
 test = test.replace("    ", "\t")
 texts = [test] * BATCH_SIZE
-encoded_pres, encoded_frags, encoded_posts = tok.encode(texts, None)
 
-mod = model.CSFP(784, tok.get_vocab_size(), 10, 512, 2, 4)
-lpc, lpf = mod(encoded_pres, encoded_frags, encoded_posts)
-
-# TODO: loss func:
-
+mod = model.CSFP(800, tok.get_vocab_size(), 10, 500, 10, 10)
+opt = optim.Adam(mod.parameters(), lr=3e-4,betas=(0.9,0.98),eps=1e-6,weight_decay=0.0)
 loss_context = torch.nn.CrossEntropyLoss()
 loss_fragment = torch.nn.CrossEntropyLoss()
 
-ground_truth = torch.arange(BATCH_SIZE)
+for e in range(EPOCHS):
+	opt.zero_grad()
+	encoded_pres, encoded_frags, encoded_posts = tok.encode(texts, None)
 
-print(ground_truth)
+	lpc, lpf = mod(encoded_pres, encoded_frags, encoded_posts)
+	ground_truth = torch.arange(BATCH_SIZE)
 
-loss = (loss_context(lpc, ground_truth) + loss_fragment(lpf, ground_truth))/2
-
-print(loss)
+	loss = (loss_context(lpc, ground_truth) + loss_fragment(lpf, ground_truth))/2
+	print(loss)
 
 
 
