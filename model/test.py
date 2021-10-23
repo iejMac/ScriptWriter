c15fbcff4778aef254e6f915f8ff1c42969e85f3
iejMac <kilianmaciej6@gmail.com>
2021-10-23 14:29:07 -0700

    training: loss func progress

--- model/test.py
+++ model/test.py
@@ -24,18 +24,31 @@ with open(test_script_name, "r") as f:
 PRE_LEN = 10
 FRAG_LEN = 10
 POST_LEN = 10
+BATCH_SIZE = 5
 
 tok = ScriptTokenizer("../data/bert-base-uncased-vocab.txt", pre_length=PRE_LEN, frag_length=FRAG_LEN, post_length=POST_LEN)
 
 test = test.replace("    ", "\t")
-texts = [test, test]
+texts = [test] * BATCH_SIZE
 encoded_pres, encoded_frags, encoded_posts = tok.encode(texts, None)
 
 mod = model.CSFP(784, tok.get_vocab_size(), 10, 512, 2, 4)
 lpc, lpf = mod(encoded_pres, encoded_frags, encoded_posts)
 
-print(lpc)
-print(lpf)
+# TODO: loss func:
+
+loss_context = torch.nn.CrossEntropyLoss()
+loss_fragment = torch.nn.CrossEntropyLoss()
+
+ground_truth = torch.arange(BATCH_SIZE)
+
+print(ground_truth)
+
+loss = (loss_context(lpc, ground_truth) + loss_fragment(lpf, ground_truth))/2
+
+print(loss)
+
+
 
 
 
